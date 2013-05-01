42b83b218992fc4d0758b3df35bf5c9a9397e29b
amaliavillanueva@gmail.com <amaliavillanueva@gmail.com@0d420f13-8156-5f95-cc0c-030a4497b76a>
2013-05-01 21:32:01 +0000

    constants

--- c++/src/Constants.cpp
+++ c++/src/Constants.cpp
@@ -1,7 +1,7 @@
 #include "Constants.h"
 
 const string Constants::MEMORY_BOARD = "board";
-const string Constants::MEMORY_PLAYER_LOSER_OF_ROUND = "./src/tablero.cpp";
+const string Constants::MEMORY_SCORE = "score";
+const string Constants::MEMORY_PLAYER_LOSER_OF_ROUND = "ronda";
 const string Constants::MEMORY_PLAYER_LOSER_OF_GAME = "juego";
 const string Constants::SEMAPHORE = "./src/tablero.cpp";
-const string Constants::MEMORY_PID_OBSERVER_BOARD = "observer";
--- c++/src/Constants.h
+++ c++/src/Constants.h
@@ -16,9 +16,9 @@ public:
 
 	static const int THERE_IS_NOT_LOSER = -1;
 	static const string MEMORY_BOARD;
+	static const string MEMORY_SCORE;
 	static const string MEMORY_PLAYER_LOSER_OF_ROUND;
 	static const string MEMORY_PLAYER_LOSER_OF_GAME;
-	static const string MEMORY_PID_OBSERVER_BOARD;
 	static const string SEMAPHORE;
 
 };
