f29e049a7a082654df880046d3f6c8ac3b7d6258
amaliavillanueva@gmail.com <amaliavillanueva@gmail.com@0d420f13-8156-5f95-cc0c-030a4497b76a>
2013-08-15 02:51:29 +0000

    test para el drag de una carta

---
+++ game/regiment/test/test/DragCarta.java
@@ -0,0 +1,38 @@
+package test;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import regiment.evento.ManejadorDeEventosDeCarta;
+
+import modelo.carta.Carta;
+import modelo.carta.PaloDeCartaInglesa;
+
+import vista.carta.CartaGrafica;
+import vista.carta.CartaInglesaGrafica;
+
+@SuppressWarnings("serial")
+public class DragCarta extends JPanel {
+	
+	public DragCarta() {
+		setLayout(null);
+		
+		CartaGrafica carta = new CartaInglesaGrafica( new Carta(8, PaloDeCartaInglesa.CORAZON) );
+		
+		carta.addManejadorDeEventos( new ManejadorDeEventosDeCarta(carta) ) ; 
+		carta.setLocation(99, 93);
+		add(carta);
+	}
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.getContentPane().add(new DragCarta());
+		
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+}
