b7c46837c39e4209fa17ec80ec8a3f7040496347
amaliavillanueva@gmail.com <amaliavillanueva@gmail.com@0d420f13-8156-5f95-cc0c-030a4497b76a>
2014-01-12 03:19:09 +0000

    delete c++ folder

--- c++/src/Constants.cpp
+++
@@ -1,7 +0,0 @@
-#include "Constants.h"
-
-const string Constants::MEMORY_BOARD = "board";
-const string Constants::MEMORY_SCORE = "score";
-const string Constants::MEMORY_PLAYER_LOSER_OF_ROUND = "ronda";
-const string Constants::MEMORY_PLAYER_LOSER_OF_GAME = "juego";
-const string Constants::SEMAPHORE = "./src/tablero.cpp";
